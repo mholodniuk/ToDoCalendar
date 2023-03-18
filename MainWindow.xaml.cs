4e940c69ccd6695f5521eed84233f9478c3d6fd0
mholodniuk <259228@student.pwr.edu.pl>
2023-03-18 12:20:48 +0100

    add base frontendadd button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ToDoCalendar
 {
@@ -25,13 +13,36 @@ namespace ToDoCalendar
             InitializeComponent();
         }
 
-        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
-            {
-                lstNames.Items.Add(txtName.Text);
-                txtName.Clear();
-            }
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void lblNote_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtNote.Focus();
+        }
+
+        private void lblTime_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtTime.Focus();
+        }
+
+        private void txtNote_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtNote.Text) && txtNote.Text.Length > 0)
+                lblNote.Visibility = Visibility.Collapsed;
+            else
+                lblNote.Visibility = Visibility.Visible;
+        }
+
+        private void txtTime_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtTime.Text) && txtTime.Text.Length > 0)
+                lblTime.Visibility = Visibility.Collapsed;
+            else
+                lblTime.Visibility = Visibility.Visible;
         }
     }
 }
